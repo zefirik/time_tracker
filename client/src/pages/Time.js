bcdb44b93f8d67faa8ae2660f63d2d214fc99469
Artem Mialo <timredrick1@gmail.com>
2021-09-10 18:36:14 +0300

    add timer

--- client/src/pages/Time.js
+++ client/src/pages/Time.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useStopwatch } from 'react-timer-hook';
+import {Button,
+        Container} from 'react-bootstrap';
 
+        function MyStopwatch() {
+          const {
+            seconds,
+            minutes,
+            hours,
+            days,
+            isRunning,
+            start,
+            pause,
+            reset,
+          } = useStopwatch({ autoStart: false });
+
+      return (
+        <Container>
+        <div className='text-center'>
+          <h1>TIMER</h1>
+          <div style={{fontSize: '100px'}}>
+            <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+          </div>
+          <p>{isRunning ? 'Running' : 'Not running'}</p>
+          <Button variant="outline-primary" onClick={start}>Start</Button>
+          <Button variant="outline-dark" onClick={pause}>Pause</Button>
+          <Button variant="outline-primary" onClick={reset}>Reset</Button>
+        </div>
+        </Container>
+      );
+    }  
+
+
+  
 function Time() {
   return (
-    <div className='products'>
-      <h1>Time</h1>
+    <div>
+      <MyStopwatch />
     </div>
   );
 }
